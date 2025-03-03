0a8826cb929bab7f997325adf75b257e78899d38
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-03-03 17:46:26 +0530

    dsd

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div></div>
+    <section className="section-about container">
+      <h2>Here are the Interesting Facts
+    <br />
+    we're proud of
+    </h2>
+
+    <div className="gradient-cards">
+      <div className="card">
+         <div className="container bg-blue-box">
+          <p className="card-title">India</p>
+          <p><span className="card-description"></span></p>
+         </div>
+      </div> 
+    </div>
+    </section>
+
   )
 }
