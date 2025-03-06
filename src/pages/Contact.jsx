d2ceef7b7291edcda7d370b62300de933cc28df3
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-03-06 13:14:50 +0530

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from "react";
 
 export const Contact = () => {
+  const handleFormSubmit = (formData) => {
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  };
   return (
-    <div></div>
-  )
-}
-
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter your name"
+            name="username"
+            required
+            autoComplete="false"
+          />
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter your email"
+            name="email"
+            required
+            autoComplete="false"
+          />
+          <textarea
+            className="form-control"
+            rows="7"
+            placeholder="Enter your name"
+            name="message"
+            required
+            autoComplete="false"
+          ></textarea>
+          <button type="submit" value="send">send</button>
+        </form>
+      </div>
+    </section>
+  );
+};
